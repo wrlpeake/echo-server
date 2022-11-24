4b4c5419ca32173c2bb22121673de378de5c6588
wrlpeake <wrlpeake@gmail.com>
2022-11-24 16:27:21 +0000

    Addresses `compileTimeError`on `ClientSocket.java` by moving `return` statement out of the `try/catch` block

--- src/main/java/org/example/ClientSocket.java
+++ src/main/java/org/example/ClientSocket.java
@@ -7,10 +7,13 @@ import java.net.Socket;
 public class ClientSocket {
 
     public Socket socketCreator(ServerSocket socket) {
+        Socket clientSocket;
         try {
-            return socket.accept();
+            clientSocket = socket.accept();
+            System.out.println("--- Connected to echo server ---");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return clientSocket;
     }
 }
